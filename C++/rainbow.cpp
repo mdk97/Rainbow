e8e89e858887afcb248bf87427613d01a509f6ae
Bruno Sacconi Peres <bruno.sacconi@spacesheepgames.com>
2021-05-01 12:43:38 -0300

    Initial support for NO_COLOR environment variable

--- C++/rainbow.cpp
+++ C++/rainbow.cpp
@@ -1,14 +1,20 @@
 #include "rainbow.hpp"
+#include <cstdlib>
 
 using std::cout;
 
 void print_colored(const string &str, const char *color_fg, const char *color_bg)
 {
-    cout << color_fg << color_bg << str << DEFAULT << BG_DEFAULT;
+    if (std::getenv("NO_COLOR") != nullptr)
+    {
+        cout << color_fg << color_bg;
+    }
+
+    cout << str << DEFAULT << BG_DEFAULT;
 }
 
 void set_color(const char *color_fg, const char *color_bg)
 {
-    printf("%s%s", color_fg, color_bg);
-    cout << color_fg << color_bg;
+    if (std::getenv("NO_COLOR") != nullptr)
+        cout << color_fg << color_bg;
 }
--- C/rainbow.c
+++ C/rainbow.c
@@ -1,11 +1,18 @@
 #include "rainbow.h"
+#include <stdlib.h>
 
 void print_colored(const char *string, const char *color_fg, const char *color_bg)
 {
-    printf("%s%s%s%s%s", color_fg, color_bg, string, DEFAULT, BG_DEFAULT);
+    if (getenv("NO_COLOR") != NULL)
+    {
+        printf("%s%s", color_fg, color_bg);
+    }
+
+    printf("%s%s%s", string, DEFAULT, BG_DEFAULT);
 }
 
 void set_color(const char *color_fg, const char *color_bg)
 {
-    printf("%s%s", color_fg, color_bg);
+    if (getenv("NO_COLOR") != NULL)
+        printf("%s%s", color_fg, color_bg);
 }
